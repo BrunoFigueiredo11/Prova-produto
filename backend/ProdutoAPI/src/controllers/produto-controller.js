54082f580d34ff5b311be5c83aba51dd76d203f4
Bruno Figueiredo Sousa <brunofigueiredo11@hotmail.com.br>
2022-09-28 16:13:48 -0300

    atualizacao envio de email

--- backend/ProdutoAPI/src/controllers/produto-controller.js
+++ backend/ProdutoAPI/src/controllers/produto-controller.js
@@ -1,5 +1,5 @@
 const { response } = require('express');
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 const mongoose = require('mongoose');
 const repository = require('../repositories/produto-repository')
 
