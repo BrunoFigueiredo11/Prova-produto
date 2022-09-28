81c5645db04cdfeca9c5b8326478eee91bbe2793
Bruno Figueiredo Sousa <brunofigueiredo11@hotmail.com.br>
2022-09-28 16:02:00 -0300

    atualizacao

--- backend/src/controllers/produto-controller.js
+++ backend/src/controllers/produto-controller.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const fetch = require('node-fetch')
 const mongoose = require('mongoose');
 const repository = require('../repositories/produto-repository')
 
@@ -16,9 +17,33 @@ exports.post = async (req, res, next) => {
     try {
         await repository.create(req.body);
         res.status(201).send({ message: "Criado com sucesso!" });
+        enviarEmail(req.body).then((data) => {
+            console.log(data);
+        })
+
     } catch (error) {
         res.status(404).send({ message: "Erro ao Enviar os Dados!" });
     }
+}
+
+async function enviarEmail(req) {
+    data = {
+        "emailFrom": "brunofigueiredo1120@gmail.com",
+        "emailTo": "brunofigueiredo1120@gmail.com",
+        "subject": "Produto Cadastrado",
+        "text": "Este email foi enviado a partir da API de produto" + req
+    }
+    const response = await fetch('http://localhost:8080/send-email', {
+        method: 'POST',
+
+        headers: {
+            'Content-Type': 'application/json'
+
+        },
+
+        body: JSON.stringify(data)
+    });
+    return response.json();
 }
 
 exports.put = async (req, res, next) => {
