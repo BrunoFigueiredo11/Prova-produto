ef9f49f1fa88f3ff7de74ae65e22877eb51d96d6
Bruno Figueiredo Sousa <brunofigueiredo11@hotmail.com.br>
2022-10-13 00:08:02 -0300

    atualizacao

--- backend/EnvioEmail/src/main/java/com/servicoemail/servicoEmail/repository/EmailRepository.java
+++ backend/EnvioEmail/src/main/java/com/servicoemail/servicoEmail/repository/EmailRepository.java
@@ -1,14 +1,13 @@
 package com.servicoemail.servicoEmail.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
-
 import com.servicoemail.servicoEmail.model.EmailModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+@Repository
 public interface EmailRepository extends MongoRepository<EmailModel, String>{
 
 
